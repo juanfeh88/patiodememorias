74d1570e7940f82ef8166422c1a7640c32118b5e
juanfeh88 <piperock92@gmail.com>
2025-07-16 16:46:58 -0500

    Updatre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import Footer from '@/components/Footer'
 import { ReactNode } from 'react'
 import { Cormorant_Garamond } from 'next/font/google'
 import { Quicksand } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const cormorant = Cormorant_Garamond({ subsets: ['latin'], weight: '700' })
 const quicksand = Quicksand({ subsets: ['latin'], weight: ['300'] })
@@ -18,6 +20,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="es" className={quicksand.className}>
       <body>
         <Header />
+        <SpeedInsights />
+        <Analytics />
         <main style={{ paddingTop: '50px' }}>{children}</main>
       </body>
     </html>
